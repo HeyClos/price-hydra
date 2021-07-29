0741669c2f8da7dff58b34c8431bd56a7da3c33f
Carlos de Santiago <los@pololu.internal>
2021-07-29 15:36:11 -0700

    added a dummy placeholder axios get request at App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import './App.css';
 
 import Prices from "./Prices";
 
+// The REST API endpoint
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 function App() {
-  const [colorList, setColorList] = useState([]);
+  // At the beginning, posts is an empty array
+  const [posts, setPosts] = useState([]);
+
+  // Define the function that fetches the data from API
+  const fetchData = async () => {
+    const { data } = await axios.get(API_URL);
+    setPosts(data);
+  };
+
+  // Trigger the fetchData after the initial render by using the useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
@@ -23,12 +38,25 @@ function App() {
           Learn React
         </a>
       </header>
-      <p>
-        Price data to be rendered below.
-      </p>
-      <Prices prices={priceData} updatePrices={setPriceData} />
+      <div className="wrapper">
+        <p>
+          Price data to be rendered below.
+        </p>
+        <Prices prices={priceData} updatePrices={setPriceData} />
+        {posts.length > 0 ? (
+          <div className="content">
+            {posts.map((post) => (
+              <div className="post">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="loading">Loading... </p>
+        )}
+      </div>
     </div>
   );
 }
-
 export default App;
