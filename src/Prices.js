d3c81647e7ec69a0f3676820813b3c10fcd88565
Carlos de Santiago <los@pololu.internal>
2021-08-04 14:38:44 -0700

    cleaned unused imports, added a greeting on app.js

--- src/Prices.js
+++ src/Prices.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { Puppeteer } from "puppeteer";
+
 //const puppeteer = require('puppeteer');
 
 const Prices = () => {
